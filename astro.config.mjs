bac811884d50eea81d504de4ed740814374a0205
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2025-09-24 16:20:46 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://int-oid.github.io/",
+  site: "https://int-oid.github.io/registrar",
   base: "/registrar",
     vite: {
     plugins: [tailwindcss()],
